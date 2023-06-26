27e42436b427bac863528d5e819c2b16f5a636ab
7Cedars <Cedars7@proton.me>
2023-06-26 12:33:22 +0100

    Exercise 7.3

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -2,20 +2,17 @@
 import { useState } from 'react'
 // import Menu from './components/Menu'
 import About from './components/About'
+import Menu from './components/Menu'
 import CreateNew from './components/CreateNew'
 import AnecdoteList from './components/AnecdoteList'
 import Anecdote from './components/Anecdote'
 import Footer from './components/Footer'
+import Notification from './components/Notification'
 
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  useMatch
-  // Navigate,
-  // useParams,
-  // useNavigate,
+  useMatch,
 } from "react-router-dom"
 
 const App = () => {
@@ -39,45 +36,19 @@ const App = () => {
   const match = useMatch('/anecdotes/:id')
   const anecdote = match 
     ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
-    : null
-  
-    
-  const Menu = () => {
-    const padding = {
-      paddingRight: 5
-    }
-    return (
-      <div>
-          <div>
-            <Link style={padding} to="/">home</Link>
-            <Link style={padding} to="/anecdotes">anecdotes</Link>
-            <Link style={padding} to="/create">create new</Link>
-            <Link style={padding} to="/about">about</Link>
-  
-            {/* <a href='#' style={padding}>anecdotes</a>
-            <a href='#' style={padding}>create new</a>
-            <a href='#' style={padding}>about</a> */}
-          </div>
-          <Routes>
-            <Route path="/anecdotes" element={<AnecdoteList anecdotes={ anecdotes } />} />
-            <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
-            <Route path="/create" element={<CreateNew/>} />
-            <Route path="/about" element={<About /> } />
-            <Route path="/" element={<AnecdoteList anecdotes={ anecdotes }/>} />
-          </Routes>
-      </div>
-    )
-  }
+    : null  
 
-  const [notification, setNotification] = useState('')
+  const [notification, setNotification] = useState(null)
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
+    setNotification(anecdote.content) 
+    setTimeout(() => { setNotification(null)}, 5000) 
   }
-
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
   
   const vote = (id) => {
     const anecdote = anecdoteById(id)
@@ -94,6 +65,14 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
+      { notification ? <Notification notification = {notification}/> : null } 
+      <Routes>
+        <Route path="/anecdotes" element={<AnecdoteList anecdotes={ anecdotes } />} />
+        <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
+        <Route path="/create" element= {<CreateNew addNew = {addNew} /> } /> 
+        <Route path="/about" element={<About /> } />
+        <Route path="/" element={<AnecdoteList anecdotes={ anecdotes }/>} />
+      </Routes>
       <Footer />
     </div>
   )
---
+++ routed-anecdotes/src/components/Notification.js
@@ -0,0 +1,7 @@
+const Notification = ( {notification} ) => (
+  <div style = {{marginTop: ".5em"}}>
+    {`A new anecdote '${notification}' created!`} 
+  </div>
+)
+
+export default Notification
