a06202cf85af47c95f6d1512f8e82b290be1e231
7Cedars <Cedars7@proton.me>
2023-06-28 09:23:12 +0100

    Exercise 7.8 update

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -52,36 +52,36 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  console.log("notes: ",notes)
-
   const handleNoteSubmit = (event) => {
-    //event.preventDefault()
+    event.preventDefault()
     noteService.create({ content: content.value })
+    console.log("notes: ", notes)
   }
  
   const handlePersonSubmit = (event) => {
-    //event.preventDefault()
+    event.preventDefault()
     personService.create({ name: name.value, number: number.value})
+    console.log("Persons: ", persons)
   }
 
-    return (
-      <div>
-        <h2>notes</h2>
-        <form onSubmit={handleNoteSubmit}>
-          <input {...content} />
-          <button>create</button>
-        </form>
-          {notes.map(n => <p key={n.id}>{n.content}</p>)}
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+        {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
-        <h2>persons</h2>
-        <form onSubmit={handlePersonSubmit}>
-          name <input {...name} /> <br/>
-          number <input {...number} />
-          <button>create</button>
-        </form>
-          {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
-      </div>
-    )
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+        {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
 }
 
 export default App
